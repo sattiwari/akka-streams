2d0c13ffa764ca575a76fcf933823f894e0a8fca
Satendra Tiwari <sattiwari18@gmail.com>
2017-03-15 15:23:05 +0700

    protocol stack demo

--- README.md
+++ README.md
@@ -13,5 +13,7 @@
 * Bi-directional flow
 
 ## Examples
-* 
+* Build a protocol stack which is composed of two stages - codec stage and framing stage. Codec stage serializes outgoing messages and deserializes incoming octet streams.
+Framing stage could add a framing protocol that attaches a length header to outgoing data and parses incoming frames back into the original octet stream chunks. These two stages are meant
+to be composed, applying one atop the other as part of a protocol stack.
 
--- src/main/scala/stlabs/shapes/BidiFlowDemo.scala
+++ src/main/scala/stlabs/shapes/BidiFlowDemo.scala
@@ -2,16 +2,21 @@ package stlabs.shapes
 
 import java.nio.ByteOrder
 
-import akka.stream.BidiShape
+import akka.stream._
 import akka.stream.scaladsl._
+import akka.stream.stage._
 import akka.util.ByteString
+import stlabs.BaseApp
+
+import scala.concurrent.Await
+import scala.concurrent.duration._
 
 trait Message
 case class Ping(id: Int) extends Message
 case class Pong(id: Int) extends Message
 
 //a graph that has exactly two open inlets and two open outlets
-object BidiFlowDemo {
+object BidiFlowDemo extends App with BaseApp {
 
   def toBytes(msg: Message): ByteString = {
     implicit val order = ByteOrder.LITTLE_ENDIAN
@@ -43,4 +48,88 @@ object BidiFlowDemo {
   // this is the same as the above
   val codec = BidiFlow.fromFunctions(toBytes _, fromBytes _)
 
+  val framing = BidiFlow.fromGraph(GraphDSL.create() { b =>
+    implicit val order = ByteOrder.LITTLE_ENDIAN
+
+    def addLengthHeader(bytes: ByteString) = {
+      val len = bytes.length
+      ByteString.newBuilder.putInt(len).append(bytes).result()
+    }
+
+    class FrameParser extends GraphStage[FlowShape[ByteString, ByteString]] {
+
+      val in = Inlet[ByteString]("FrameParser.in")
+      val out = Outlet[ByteString]("FrameParser.out")
+      override val shape = FlowShape.of(in, out)
+
+      override def createLogic(inheritedAttributes: Attributes): GraphStageLogic = new GraphStageLogic(shape) {
+
+        // this holds the received but not yet parsed bytes
+        var stash = ByteString.empty
+        // this holds the current message length or -1 if at a boundary
+        var needed = -1
+
+        setHandler(out, new OutHandler {
+          override def onPull(): Unit = {
+            if (isClosed(in)) run()
+            else pull(in)
+          }
+        })
+        setHandler(in, new InHandler {
+          override def onPush(): Unit = {
+            val bytes = grab(in)
+            stash = stash ++ bytes
+            run()
+          }
+
+          override def onUpstreamFinish(): Unit = {
+            if (stash.isEmpty) completeStage()
+            // wait with completion and let run() complete when the
+            // rest of the stash has been sent downstream
+          }
+        })
+
+        private def run(): Unit = {
+          if (needed == -1) {
+            // are we at a boundary? then figure out next length
+            if (stash.length < 4) {
+              if (isClosed(in)) completeStage()
+              else pull(in)
+            } else {
+              needed = stash.iterator.getInt
+              stash = stash.drop(4)
+              run() // cycle back to possibly already emit the next chunk
+            }
+          } else if (stash.length < needed) {
+            // we are in the middle of a message, need more bytes,
+            // or have to stop if input closed
+            if (isClosed(in)) completeStage()
+            else pull(in)
+          } else {
+            // we have enough to emit at least one message, so do it
+            val emit = stash.take(needed)
+            stash = stash.drop(needed)
+            needed = -1
+            push(out, emit)
+          }
+        }
+      }
+    }
+
+    val outbound = b.add(Flow[ByteString].map(addLengthHeader))
+    val inbound = b.add(Flow[ByteString].via(new FrameParser))
+    BidiShape.fromFlows(outbound, inbound)
+  })
+
+  val stack = codec.atop(framing)
+
+  // test it by plugging it into its own inverse and closing the right end
+  val pingpong = Flow[Message].collect { case Ping(id) => Pong(id) }
+  val flow = stack.atop(stack.reversed).join(pingpong)
+  val result = Source((0 to 9).map(Ping)).via(flow).limit(20).runWith(Sink.seq)
+  val x = Await.result(result, 1.second)
+
+  x.foreach(println)
+
+  system.shutdown()
 }
