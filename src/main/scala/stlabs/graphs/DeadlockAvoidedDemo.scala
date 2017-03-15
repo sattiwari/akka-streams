d0606f05e7249d89fa8ad31d1db8e595689e08e1
Satendra Tiwari <sattiwari18@gmail.com>
2017-03-15 16:12:18 +0700

    avoid deadlock using preferred merged

--- src/main/scala/stlabs/graphs/DeadlockAvoidedDemo.scala
+++ src/main/scala/stlabs/graphs/DeadlockAvoidedDemo.scala
@@ -1,22 +1,23 @@
 package stlabs.graphs
 
 import akka.stream.ClosedShape
-import akka.stream.scaladsl._
+import akka.stream.scaladsl.{Broadcast, Flow, GraphDSL, Merge, MergePreferred, RunnableGraph, Sink}
 import stlabs.BaseApp
 
-object DeadlockDemo extends BaseApp with App {
+object DeadlockAvoidedDemo extends BaseApp with App {
 
   RunnableGraph.fromGraph(GraphDSL.create() { implicit b =>
     import GraphDSL.Implicits._
 
-    val merge = b.add(Merge[Int](2))
+    val merge = b.add(MergePreferred[Int](1))
     val bcast = b.add(Broadcast[Int](2))
 
     source ~> merge ~> Flow[Int].map { s => println(s); s } ~> bcast ~> Sink.ignore
-    merge                    <~                      bcast
+    merge.preferred                    <~                      bcast
     ClosedShape
   }).run()
 
   Thread.sleep(1000)
   system.shutdown()
 }
+
